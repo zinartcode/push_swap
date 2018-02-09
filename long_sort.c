4808614bd8ae9bcfad0ba95347fd8eaf25ea4d10
Artur Zinnatullin <azinnatu@e1z3r5p20.42.us.org>
2018-02-08 19:29:18 -0800

    sorts 100 in 725

--- long_sort.c
+++ long_sort.c
@@ -6,7 +6,7 @@
 /*   By: azinnatu <azinnatu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/29 17:12:32 by azinnatu          #+#    #+#             */
-/*   Updated: 2018/02/08 00:41:47 by azinnatu         ###   ########.fr       */
+/*   Updated: 2018/02/08 19:05:59 by azinnatu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "push_swap.h"
@@ -16,38 +16,36 @@ void	b_to_a_head(t_stack *stack)
 	while (size(stack->b) > stack->count/2)
 	{
 		while (index_in_list(stack->b, big(stack->b)) < size(stack->b)/2)
-		{
-			if(stack->b->data < stack->b->next->data)
-				sb(stack);
-			while (stack->b->data != big(stack->b))
-			{
-				rb(stack);
-				stack->i++;
-			}
-			pa(stack);
-		}
+			check_big(stack);
 		shift_b_back(stack);
 	}
 	stack->i = 0;
 }
 
+void	check_big(t_stack *stack)
+{
+	stack->temp = second_big(stack->b);
+	check_head_b(stack);
+	while (stack->b->data != big(stack->b))
+	{
+		if (stack->b->data == stack->temp)
+			pa(stack);
+		else
+		{
+			rb(stack);
+			stack->i++;
+		}
+	}
+	pa(stack);
+	check_head_a(stack);	
+}
+
 void	shift_b_back(t_stack *stack)
 {
-	while (stack->i > 0)
+	while (stack->b && stack->i > 0)
 	{
-		if (stack->b->data == big(stack->b))
-			pa(stack);
 		if (index_in_list(stack->b, big(stack->b)) < size(stack->b)/2)
-		{
-			while (stack->b->data != big(stack->b))
-			{
-				rb(stack);
-				stack->i++;
-			}
-			pa(stack);
-		}
-		if (stack->a->data > stack->a->next->data)
-			sa(stack);
+			check_big(stack);
 		else
 		{
 			rrb(stack);
@@ -58,101 +56,118 @@ void	shift_b_back(t_stack *stack)
 
 void	b_to_a_tail(t_stack *stack)
 {
-	stack->i = 0;
-	while (stack->b)
+	while (size(stack->b) > 3)
 	{
-		ft_tail1(stack);
-		ft_tail2(stack);
-		if (size(stack->b) <= 3)
-		{
-			if (stack->b && last(stack->b) == big(stack->b))
-				rrb(stack);
-			while (stack->b && stack->b->data != big(stack->b))
-				rb(stack);
-			pa(stack);
-		}
+		while (index_in_list(stack->b, big(stack->b)) < size(stack->b)/2)
+			check_big(stack);
+		shift_b_back(stack);
 	}
-}
-
-void	ft_tail1(t_stack *stack)
-{
-	if (index_in_list(stack->b, big(stack->b)) < size(stack->b)/2)
+	if (size(stack->b) <= 3)
 	{
-		if(stack->b->data < stack->b->next->data)
-			sb(stack);
-		while (stack->b->data != big(stack->b))
-		{
-			rb(stack);
-			stack->i++;
-		}
-		pa(stack);	
-	}
-	if (index_in_list(stack->b, big(stack->b)) > size(stack->b)/2)
-	{
-		while (stack->b->data != big(stack->b))
-		{
+		if (stack->b && last(stack->b) == big(stack->b))
 			rrb(stack);
-			stack->i--;
-		}
+		while (stack->b && stack->b->data != big(stack->b))
+			rb(stack);
+		pa(stack);
 		pa(stack);
 	}
 }
 
-void	ft_tail2(t_stack *stack)
-{
-	while (stack->i > 0 && size(stack->b) > 3)
-	{
-		if (stack->b->data == big(stack->b))
-			pa(stack);
-		if (index_in_list(stack->b, big(stack->b)) <= size(stack->b)/2)
-		{
-			while (stack->b->data != big(stack->b))
-			{
-				rb(stack);
-				stack->i++;
-			}
-			pa(stack);
-		}
-		if (stack->a->data > stack->a->next->data)
-			sa(stack);
-		else
-		{
-			rrb(stack);
-			stack->i--;
-		}
-	}	
-}
-
-
-// void	long_sort(t_stack *stack)
+// void	b_to_a_tail(t_stack *stack)
 // {
-// 	 int	i;
-// 	 int	pivot;
-// 	 int	mid;
+// 	stack->i = 0;
+// 	while (stack->b)
+// 	{
+// 		ft_tail1(stack);
+// 		ft_tail2(stack);
+// 		if (size(stack->b) <= 3)
+// 		{
+// 			if (stack->b && last(stack->b) == big(stack->b))
+// 				rrb(stack);
+// 			while (stack->b && stack->b->data != big(stack->b))
+// 				rb(stack);
+// 			pa(stack);
+// 		}
+// 	}
+// }
 
-// 	 i = 0;
-// 	 mid = stack->count;
-// 	 while (mid != 0)
-// 	 {
-// 	 	pivot = mid/2;
-// 	 	if (index_in_list(stack->a, sml(stack->a)) < pivot)
-// 	 	{
-// 	 		while (stack->a && stack->a->data != sml(stack->a))
-// 	 		{
-// 	 			ra(stack);
-// 	 		}
-// 	 		pb(stack);
-// 	 	}
-// 	 	else
-// 	 	{
-// 	 		while (last(stack->a) != sml(stack->a))
-// 	 		{
-// 	 			rra(stack);
-// 	 		}
-// 	 		rra(stack);
-// 	 		pb(stack);
-// 	 	}
-// 	 	mid--;
-// 	 }
+// void	ft_tail1(t_stack *stack)
+// {
+// 	if (index_in_list(stack->b, big(stack->b)) < size(stack->b)/2)
+// 	{
+// 		stack->temp = second_big(stack->b);
+// 		check_head_b(stack);
+// 		while (stack->b->data != big(stack->b))
+// 		{
+// 			if (stack->b->data == stack->temp)
+// 				pa(stack);
+// 			else
+// 			{
+// 				rb(stack);
+// 				stack->i++;
+// 			}
+// 		}
+// 		pa(stack);
+// 		check_head_a(stack);	
+// 	}
+// 	if (index_in_list(stack->b, big(stack->b)) > size(stack->b)/2)
+// 	{
+// 		while (stack->b->data != big(stack->b))
+// 		{
+// 			rrb(stack);
+// 			stack->i--;
+// 		}
+// 		pa(stack);
+// 	}
+// }
+
+// void	ft_tail2(t_stack *stack)
+// {
+// 	while (size(stack->b) > 3 && stack->i > 0)
+// 	{
+// 		if (stack->b->data == big(stack->b))
+// 			pa(stack);
+// 		if (index_in_list(stack->b, big(stack->b)) <= size(stack->b)/2)
+// 		{
+// 			while (stack->b->data != big(stack->b))
+// 			{
+// 				rb(stack);
+// 				stack->i++;
+// 			}
+// 			pa(stack);
+// 		}
+// 		if (stack->a->data > stack->a->next->data)
+// 			sa(stack);
+// 		else
+// 		{
+// 			rrb(stack);
+// 			stack->i--;
+// 		}
+// 	}	
+// }
+
+// void	shift_b_back(t_stack *stack)
+// {
+// 	while (stack->i > 0)
+// 	{
+// 		if (stack->b->data == big(stack->b))
+// 			pa(stack);
+// 		if (index_in_list(stack->b, big(stack->b)) < size(stack->b)/2)
+// 		{
+// 			while (stack->b->data != big(stack->b))
+// 			{
+// 				rb(stack);
+// 				stack->i++;
+// 			}
+// 			pa(stack);
+// 		}
+// 		if (stack->a->data > stack->a->next->data)
+// 			sa(stack);
+// 		else
+// 		{
+// 			rrb(stack);
+// 			stack->i--;
+// 		}
+// 	}
 // }
 
